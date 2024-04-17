6939944654a06d17825935ae0116e4b240b13fc5
Brian-mula <mulatiwangilabrian@gmail.com>
2024-04-17 15:08:40 +0300

    fix: tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,10 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule,
-    MongooseModule.forRoot('mongodb+srv://mulati:Wangila2000@cluster0.s58rx.mongodb.net/spribe')
+    MongooseModule.forRoot('mongodb+srv://mulati:Wangila2000@cluster0.s58rx.mongodb.net/spribe'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  constructor(@InjectModel(User.name) private userModel:Model<User>){}
    async create(createUserDto: CreateUserDto) {
-    const { username, phone_number } = createUserDto;
+    try {
+      const { username, phone_number } = createUserDto;
+    const dbUser = await this.userModel.findOne({phone_number});
+    if(dbUser){
+      throw new HttpException('User already exists', 400);
+    }
     const newUser = {
       username,
       phone_number,
     
     }
-    return newUser;
+    return await this.userModel.create(newUser);
+
+    // return newUser;
+    } catch (error) {
+      throw new HttpException(error.message, 500);
+    }
   }
 
   findAll() {
